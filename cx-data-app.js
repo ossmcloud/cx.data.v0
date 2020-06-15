687d674fed1ebc2ef664234b4a1cd080b540224b
development <development@ossmcloud.ie>
2020-06-15 23:29:32 +0100

    alpha development

--- cx-data-app.js
+++ cx-data-app.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const _cx_sql = require('./core/cx-sql-pool-manager');
+const DBContext = require('./schema/cx-context');
+
+
+
+module.exports = {
+    DBContext: DBContext,
+    getPool: _cx_sql.get,
+}
